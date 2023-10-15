4273fe0bc058d7c87bd8a6bb48d7777cd636fd4b
Meenakshi <hiringhrbot39@gmail.com>
2023-10-15 10:32:34 +0530

    array

--- array/problem13.js
+++ array/problem13.js
@@ -1,15 +1,20 @@
-
-// 👺👺doubt
+/* 👺👺doubt */
 /*Write a JavaScript program to add items to a blank array and display them.
 Sample Screen :
 add elements in an blank array
 */
 
 function add_elements_to_blank_array(array) {
-  let blankArray = [0];
+  let blankArray = [];
+  let sum = 0;
   for (let i = 0; i < array.length; i++) {
-   return blankArray[i] += array[i];
+    sum += array[i];
   }
+  
+  // console.log(sum);
+
+   blankArray.push(sum);
+   return blankArray;      /*this is how we push a value into array */
 }
 
 console.log(add_elements_to_blank_array([1, 2, 3]));
