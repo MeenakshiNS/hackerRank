811157ba95c3df9434862e927252fdee9027026b
Meenakshi <hiringhrbot39@gmail.com>
2023-10-15 10:39:29 +0530

    array

--- array/problem13.js
+++ array/problem13.js
@@ -1,4 +1,4 @@
-/* 👺👺doubt */
+/* 👺👺 */
 /*Write a JavaScript program to add items to a blank array and display them.
 Sample Screen :
 add elements in an blank array
--- array/problem14.js
+++ array/problem14.js
@@ -1,29 +1,4 @@
-// 👺 DOUBT
-// /*
-// Write a JavaScript program to remove duplicate items
-//  from an array (ignore case sensitivity).
-// */
-// function removeDuplicates(array){
-//     let uniqueElments=[];
-//     for(let i=0;i<array.length;i++){
-//         for(let j=0;j<uniqueElments.length;j++){
-//             if(array[i]===uniqueElments[j]){
-//                 continue;
-//             }else{
-//                 uniqueElments.push(array[i]);
-               
-//             }
-
-//         }
-
-//     }
-//     console.log(uniqueElments);
-
-
-
-// }
-// removeDuplicates([1,2,4,2,6,4,8,1]);
-
+/* 👺 */
 
 function removeDuplicates(array) {
     let uniqueElements = [];
@@ -32,7 +7,8 @@ function removeDuplicates(array) {
         for (let j = 0; j < uniqueElements.length; j++) {
             if (array[i] === uniqueElements[j]) {
                 isUnique = false;
-                break;
+                 continue;
+                
             }
         }
         if (isUnique) {
