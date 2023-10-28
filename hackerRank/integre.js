78bc48227ce7c415faa74cbc288b0264cee6d5c5
Meenakshi <hiringhrbot39@gmail.com>
2023-10-28 10:02:38 +0530

    hacker rank

---
+++ hackerRank/integre.js
@@ -0,0 +1,57 @@
+
+/*
+Complete the code in the editor below. The variables , , and  are already declared and initialized for you. You must:
+
+Declare  variables: one of type int, one of type double, and one of type String.
+Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your  variables.
+Use the  operator to perform the following operations:
+Print the sum of  plus your int variable on a new line.
+Print the sum of  plus your double variable to a scale of one decimal place on a new line.
+Concatenate  with the string you read as input and print the result on a new line.
+*/
+
+
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+// Reads complete line from STDIN
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
+function main() {
+    var i = 4
+    var d = 4.0
+    var s = "HackerRank "
+    // Declare second integer, double, and String variables.
+    let secondInteger=parseInt(readLine());
+    let secondDouble=parseFloat(readLine())
+    let secondString=readLine();
+    
+    console.log(secondInteger+i);
+    console.log((secondDouble+d).toFixed(1));
+    console.log(s.concat(secondString));
+
+    // Read and save an integer, double, and String to your variables.
+
+    // Print the sum of both integer variables on a new line.
+
+    // Print the sum of the double variables on a new line.
+
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+}
